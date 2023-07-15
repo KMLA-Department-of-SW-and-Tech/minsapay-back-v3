becd1f5315c2645bba35a28ea4c05e552869b001
Jinho Kim <jinojoa317@naver.com>
2023-07-15 16:56:20 +0900

    .

--- models/User.ts
+++ models/User.ts
@@ -1,6 +1,6 @@
-import { createSchema, Type, typedModel, ExtractDoc } from "ts-mongoose";
-import { PurchaseSchema } from "./Purchase";
-
+// import { createSchema, Type, typedModel, ExtractDoc } from "ts-mongoose";
+// import { PurchaseSchema } from "./Purchase";
+/*
 const UserSchema = createSchema({
   name: Type.string({ required: true }),
   purchases: Type.array().of(Type.string()),
@@ -13,4 +13,17 @@ const UserModel = typedModel("User", UserSchema, "User");
 
 type UserDoc = ExtractDoc<typeof UserSchema>;
 
-export { UserSchema, UserModel, UserDoc };
+export { UserSchema, UserModel, UserDoc };*/
+
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+  name: { type: String, required: true },
+  purchases: { type: Array, required: true },
+  balance: { type: Number, required: true },
+  isSecurePurchase: { type: Boolean, required: true },
+  securePurchaseEndDate: { type: Date, required: true },
+});
+
+export const UserModel = mongoose.model("User", UserSchema, "User");
