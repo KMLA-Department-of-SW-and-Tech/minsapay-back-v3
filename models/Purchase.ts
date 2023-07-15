2bdb114494ba82e5741138627e631154b76329b7
Jinho Kim <jinojoa317@naver.com>
2023-07-15 16:55:59 +0900

    .

--- models/Purchase.ts
+++ models/Purchase.ts
@@ -1,5 +1,5 @@
-import { createSchema, Type, typedModel, ExtractDoc } from "ts-mongoose";
-
+//import { createSchema, Type, typedModel, ExtractDoc } from "ts-mongoose";
+/*
 const PurchaseSchema = createSchema({
   time: Type.string({ required: true }),
   user: Type.string({ required: true }),
@@ -17,3 +17,20 @@ const PurchaseModel = typedModel("Purchase", PurchaseSchema, "Purchase");
 type PurchaseDoc = ExtractDoc<typeof PurchaseSchema>;
 
 export { PurchaseSchema, PurchaseModel, PurchaseDoc };
+*/
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const PurchaseSchema = new Schema({
+  time: { type: String, required: true },
+  user: { type: String, required: true },
+  store: { type: String, required: true },
+  product: { type: String, required: true },
+  price: { type: Number, required: true },
+  nameOfStore: { type: String, required: true }, 
+  nameOfUser: { type: String, required: true },
+  userAmount: { type: Number, required: true },
+  storeAmount: { type: Number, required: true },
+});
+
+export const PurchaseModel = mongoose.model("Purchase", PurchaseSchema, "Purchase");
